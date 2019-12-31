c5e66b58c6f7228f08a4a5b56dcf5580c02d49f1
wangshaofeng <1693476459@qq.com>
2019-12-31 21:23:47 +0800

    初始化webapi项目

---
+++ WebApiDoc(使用Swagger生成文档)/WebApiDoc(使用Swagger生成文档)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiDoc_使用Swagger生成文档_.WebApiApplication" Language="C#" %>
---
+++ WebApiDoc(使用Swagger生成文档)/WebApiDoc(使用Swagger生成文档)/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WebApiDoc_使用Swagger生成文档_
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
